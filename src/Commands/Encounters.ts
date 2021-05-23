45e1d7af43de8e4ea4621e1e604887b2d9413703
bjellesma <william.jellesma@gmail.com>
2021-05-23 15:57:31 -0400

    feat: encounter command will return wowhead links. TODO see if we can parse the page

--- src/Commands/Encounters.ts
+++ src/Commands/Encounters.ts
@@ -57,22 +57,71 @@ export class Encounters {
         const encounter = await this.getEncounter(encounterId);
         let embedFields: EmbedField[] = [];
         embed.setTitle(encounter.name);
-        encounter?.sections.forEach((section: any) => {
+        // a for loop will allow for await and return
+        for (let section of encounter?.sections) {
+            // if there is a spell, set the text = to a spell link
+            let spell = section?.spell;
+            if (spell) {
+                // get spell text
+                section.body_text = await this.getEncounterSpell(
+                    spell.id,
+                    spell.key.href
+                );
+            }
             embedFields.push({
                 name: section.title,
                 value: section?.body_text || 'No Text'
             });
-            if (section?.sections) {
-                section.sections.forEach((innerSection: any) => {
-                    embedFields.push({
-                        name: innerSection.title,
-                        value: innerSection?.body_text || 'No Text',
-                        inline: true
-                    });
-                });
-            }
-        });
+            let innerSections: any[] = section?.sections;
+            // recursive call to get all other subsections
+            this.parseEncounterSections(innerSections, embedFields);
+        }
+
         embed.addFields(embedFields);
         return embed;
+    }
+
+    // Recursive function to get the subsections
+    async parseEncounterSections(sections: any, embedFields: any) {
+        if (sections) {
+            for (let section of sections) {
+                // if there is a spell, set the text = to a spell link
+                let spell = section?.spell;
+                if (spell) {
+                    section.body_text = await this.getEncounterSpell(
+                        spell.id,
+                        spell.key.href
+                    );
+                }
+                embedFields.push({
+                    name: section.title,
+                    value: section?.body_text || 'No Text',
+                    inline: true
+                });
+                let innerSections: any[] = section?.sections;
+                this.parseEncounterSections(innerSections, embedFields);
+            }
+        }
+        return embedFields;
+    }
+
+    // TODO this function should be moved to the spell class
+    async getEncounterSpell(
+        spellId: number,
+        spellHref: string
+    ): Promise<string> {
+        let data = await blizzardObject.refreshBlizzardToken();
+        let accessToken = data.token;
+        let url = `${spellHref}&access_token=${accessToken}`;
+        let response: any = await fetch(url).catch((error) =>
+            console.log(`Unable to get spell. Error: ${error}`)
+        );
+        let result = await response.json();
+        // if the spell cannot be resolved
+        if (result.code == '404') {
+            //TODO parse the page on wowhead
+            return `https://www.wowhead.com/spell=${spellId}/`;
+        }
+        return result?.description;
     }
 }
